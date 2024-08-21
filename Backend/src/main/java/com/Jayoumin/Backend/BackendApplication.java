0e8436ee72378ed92e048996fefbd9b27dddbc43
0woy <qkrdbsdk1030@naver.com>
2024-08-21 17:02:11 +0900

    chore: configure CORS policy

--- Backend/src/main/java/com/Jayoumin/Backend/BackendApplication.java
+++ Backend/src/main/java/com/Jayoumin/Backend/BackendApplication.java
@@ -2,6 +2,9 @@ package com.Jayoumin.Backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class BackendApplication {
@@ -10,4 +13,16 @@ public class BackendApplication {
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
+	// Croww-Origin Resource Shairng (CORS)
+	@Bean
+	public WebMvcConfigurer corsConfigurer(){
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")	// 모든 경로에 대한 http 요청 처리
+						.allowedMethods("*")			// 모든  http 메소드 허용
+						.allowedOrigins("http://localhost:5173");	// 위 경로에서 오는 요청만 허용
+			}
+		};
+	}
 }
