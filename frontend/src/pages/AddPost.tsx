c4811c687d47b981033effc6e4bceff14835b464
Jin <munjin1340@gmail.com>
2024-08-26 01:18:09 +0200

    small changes

--- frontend/src/pages/AddPost.tsx
+++ frontend/src/pages/AddPost.tsx
@@ -1,8 +1,16 @@
-
 export default function AddPost() {
+  //   const [image, setimage] = useState('');
+  //   const [post, setpost] = useState();
+
   return (
-    <div>
-     <input type="text" placeholder='have a green day!'/>
-    </div>
-  )
+    <section>
+      <div>
+        <input type="file" />
+        <button>Upload</button>
+      </div>
+      <form action="">
+        <input type="text" placeholder="comment.." />
+      </form>
+    </section>
+  );
 }
