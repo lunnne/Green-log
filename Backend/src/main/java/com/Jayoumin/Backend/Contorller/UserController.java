4730811d8a5224bbeae720b0dfc6437a048aed49
0woy <qkrdbsdk1030@naver.com>
2024-08-26 13:15:18 +0900

    feat: http 페이지에 입력한 정보 Controller로 가져오기

--- Backend/src/main/java/com/Jayoumin/Backend/Contorller/UserController.java
+++ Backend/src/main/java/com/Jayoumin/Backend/Contorller/UserController.java
@@ -1,2 +1,25 @@
-package com.Jayoumin.Backend.Contorller;public class UserController {
+package com.Jayoumin.Backend.Contorller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class UserController {
+    @GetMapping("/user/signup")
+    public String signUp(){
+        return "signUp";
+    }
+
+    @PostMapping("/user/signup")
+    public String sighUp(@RequestParam("user_id") String user_id,
+                         @RequestParam("password") String password,
+                         @RequestParam("email") String email,
+                         @RequestParam("nickname") String nickname
+                         ){
+        System.out.println(user_id+" "+password+" "+email+" "+nickname);
+        return "sign";
+    }
+
 }
