a0a9bb0cef8cde84a25c04873f588bcf6fa635a5
0woy <qkrdbsdk1030@naver.com>
2024-08-28 13:16:05 +0900

    feat: http -> db 저장 로직 구현

--- Backend/src/main/java/com/Jayoumin/Backend/Contorller/UserController.java
+++ Backend/src/main/java/com/Jayoumin/Backend/Contorller/UserController.java
@@ -1,24 +1,31 @@
 package com.Jayoumin.Backend.Contorller;
 
+import com.Jayoumin.Backend.DTO.UserDTO;
+import com.Jayoumin.Backend.Service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
     @GetMapping("/user/signup")
     public String signUp(){
         return "signUp";
     }
 
     @PostMapping("/user/signup")
-    public String sighUp(@RequestParam("user_id") String user_id,
-                         @RequestParam("password") String password,
-                         @RequestParam("email") String email,
-                         @RequestParam("nickname") String nickname
-                         ){
-        System.out.println(user_id+" "+password+" "+email+" "+nickname);
+    public String sighUp(@ModelAttribute UserDTO userDTO){
+        System.out.println("UserController.signUp");
+        System.out.println("UserDTO = "+userDTO);
+        userService.signUp(userDTO);
         return "sign";
     }
 
