99a51de767922675e07e4a21c8466ec397ee798c
Jin <munjin1340@gmail.com>
2024-09-04 19:26:35 +0200

    feat: login

--- frontend/src/features/auth/LogIn.tsx
+++ frontend/src/features/auth/LogIn.tsx
@@ -1,6 +1,19 @@
-
 export default function LogIn() {
   return (
-    <div>Login</div>
-  )
+    <section>
+      <div>
+        <form>
+          <div className="mb-4">
+            <label htmlFor="username">Username</label>
+            <input type="username" name="username" id="username" placeholder="Username" />
+          </div>
+          <div>
+            <label htmlFor="password">Password</label>
+            <input type="password" name="password" id="password" placeholder="Username" />
+          </div>
+          <button type="submit">Log In</button>
+        </form>
+      </div>
+    </section>
+  );
 }
