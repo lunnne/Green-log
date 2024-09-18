b04fd319ea84559eb82ecfa0e8b7a4d09dc82b1f
0woy <qkrdbsdk1030@naver.com>
2024-09-18 15:41:48 +0900

    feat: Image, Post 도메인 생성

---
+++ Backend/src/main/java/com/Jayoumin/Backend/Domain/Post.java
@@ -0,0 +1,41 @@
+package com.Jayoumin.Backend.Domain;
+
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "posts")
+@Data
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    private String content;
+
+    @CreatedDate
+    private LocalDateTime create_at;
+
+    @LastModifiedDate
+    private LocalDateTime update_at;
+
+    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Image> images;
+
+    @ManyToOne(cascade = CascadeType.REMOVE)
+    @JoinColumn(name="user_id")
+    private User user;
+
+
+}
